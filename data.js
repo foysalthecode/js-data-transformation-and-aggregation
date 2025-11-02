1f6efff374142fe0899b8e8fc37b45d99f0c113b
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-02 11:30:15 +0600

    practice: Map.forEach() usage examples

---
+++ data.js
@@ -0,0 +1,27 @@
+// const obj = {
+//   nextLevel: { courseId: "level 2" },
+//   "programming hero": { courseId: "level 1" },
+// };
+// console.log(obj["programming hero"].courseId);
+
+const course1 = { name: "programmin hero" };
+const course2 = { name: "Next level web development" };
+
+const map = new Map();
+
+// map.set(1, "programming hero");
+// map.set(false, "programming hero"); //if this is also a boolean then it will remain an object
+map.set(course1, { courseId: "level 1" }); //we can set another object as a key
+map.set(course2, { courseId: "level 2" }); //we can set another object as a key
+
+// map.clear(); // for clearing everything
+// console.log(map.size); //map.size counts how many element are there in the map
+
+// map.delete(course1) // this delete method uses for delete a particular object using key
+
+// console.log(map.has(course1)); // this has method checks that is this element exist or not ..if exist it will return a boolean
+// map.forEach((value,key) =>{console.log('key:',key,'value:',value);}) // for iteration there is only foreach method works
+
+map.forEach((value, key) => (key.name = "sohoz sorol simple" + key.name));
+
+console.log(map);
