9b21316f6ba20bb00e4b32d1ffb9635ff51eb03c
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-02 13:42:20 +0600

    practice sort and localCompare sort

---
+++ sortNflattening.js
@@ -0,0 +1,22 @@
+//sort
+
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["Banana", "apple", "Cherry", "date"];
+
+// const sortedNumber = numbers.sort((a, b) => a - b);
+// console.log(sortedNumber);
+
+// fruits.sort((a, b) => a.localeCompare(b)); //sort according alphabetic order
+// console.log(fruits);
+
+//nested array flat
+
+const tagsFromPosts = [
+  ["javascript", "react", "css"],
+  ["node", "express"],
+  ["javascript", "html", "css"],
+];
+
+//some
+const currentUserRoles = ["user", "editor"];
+const featureAccessRoles = ["Admin", "Manager"];
