e91d9891095c90d438c8b9fbd9276119b251d99d
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-03 11:11:15 +0600

    Practice: Array cross matching and Array.from()

--- sortNflattening.js
+++ sortNflattening.js
@@ -1,4 +1,4 @@
-//sort
+//sort ---------------------------------------------
 
 const numbers = [40, 100, 1, 5, 25, 10];
 const fruits = ["Banana", "apple", "Cherry", "date"];
@@ -9,7 +9,7 @@ const fruits = ["Banana", "apple", "Cherry", "date"];
 // fruits.sort((a, b) => a.localeCompare(b)); //sort according alphabetic order
 // console.log(fruits);
 
-//nested array flat
+//nested array flat -----------------------------------------------
 
 const tagsFromPosts = [
   ["javascript", "react", "css"],
@@ -17,9 +17,32 @@ const tagsFromPosts = [
   ["javascript", "html", "css"],
 ];
 
-const filterdTags = [...new Set(tagsFromPosts.flat())];
-console.log(filterdTags);
+// const filterdTags = [...new Set(tagsFromPosts.flat())];
+// console.log(filterdTags);
 
-//some
-const currentUserRoles = ["user", "editor"];
-const featureAccessRoles = ["Admin", "Manager"];
+//some -----------------------------------------------
+// const numberss = [1, 2, 3, 4, 5, 6, 7];
+
+// const hasOddNumber = numberss.some((number) => number % 2 === 0);
+// console.log(hasOddNumber);
+
+// const currentUserRoles = ["user", "editor"];
+// const featureAccessRoles = ["admin", "manager"];
+
+// const canAccess = currentUserRoles.some((role) =>
+//   featureAccessRoles.includes(role)
+// );
+
+// console.log(canAccess);
+
+//Array.from() ---------------------
+// const arr = Array.from([1, 2, 3], (value, i) => value * value);
+// console.log(arr);
+
+const range = (start, stop, step) =>
+  Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * step
+  );
+
+console.log(range(0, 11, 1));
